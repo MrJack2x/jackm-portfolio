a39822856f4c1dea877f68b60e123b8dd136ffbc
Jack Michell <me@jmich.dev>
2024-05-19 16:23:58 -0500

    Updated about section paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,14 +13,13 @@ export default function About() {
           </p>
           <h2 className="py-4">Who I Am</h2>
           <p className="py-2 text-gray-600">
-            I am an aspiring software engineer based in the United States. I am
-            currently pursuing a Bachelor&#39;s degree in Computer Science in my
-            first year of college. I have a passion for creating different
-            things and problem-solving. Throughout my life, mathematics has
-            always been something I was fairly good with, and enjoyed the
-            problem-solving aspect of it. I have always been interested in
-            technology and how it works, and I want to take this passion and use
-            it to create software that will benefit the world.
+            I am a software engineer based in the United States. I have a
+            passion for creating different things and problem-solving.
+            Throughout my life, mathematics has always been something I was
+            fairly good with, and enjoyed the problem-solving aspect of it. I
+            have always been interested in technology and how it works, and I
+            want to take this passion and use it to create software that will
+            benefit the world.
           </p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 mt-4 hover:scale-105 ease-in duration-300">
