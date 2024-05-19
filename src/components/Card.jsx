4f96c0745c7cd04325b6b94e663efe83e01f765f
Jack Michell <me@jmich.dev>
2024-05-19 17:00:11 -0500

    Completed portfolio section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
-import ExampleProjectImg from "../assets/about.jpg";
+import PropTypes from "prop-types";
 
-export default function Card() {
+export default function Card({ projectName, projectImg }) {
   return (
     <div className="px-8 shadow-xl rounded-xl">
       <div className="flex justify-center items-center">
         <div className="flex flex-col items-center justify-center">
           <div className="px-8 py-4">
             <div className="flex flex-col justify-center items-center p-4">
-              <h3 className="text-lg my-4 text-center">Example Project</h3>
+              <h3 className="text-lg my-4 text-center">{projectName}</h3>
               <img
                 className="rounded-xl hover:scale-105 ease-in duration-300 h-[75px] sm:h-[200px]"
-                src={ExampleProjectImg}
-                alt="Example"
+                src={projectImg}
+                alt="Projects"
               />
             </div>
           </div>
@@ -20,3 +20,8 @@ export default function Card() {
     </div>
   );
 }
+
+Card.propTypes = {
+  projectName: PropTypes.string,
+  projectImg: PropTypes.string,
+};
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,20 +1,30 @@
 import Card from "./Card";
+import PortfolioImg from "../assets/portfolio.png";
+import HelveraImg from "../assets/helvera.png";
 
 export default function Portfolio() {
+  const portfolioName = "My Portfolio";
+  const portfolioImg = PortfolioImg;
+  const helveraName = "Helvera";
+  const helveraImg = HelveraImg;
+
   return (
     <div id="portfolio" className="w-full lg:h-screen px-8">
       <div className="max-w-[1240px] m-4 mx-auto flex flex-col justify-center h-full">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Portfolio
         </p>
-        <h2 className="py-4">Coming Soon</h2>
-        {/* TODO: Finish adding portfolio */}
-        {/*   <div className="p-8"> */}
-        {/*     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 justify-center items-center"> */}
-        {/*       <Card /> */}
-        {/*       <Card /> */}
-        {/*     </div> */}
-        {/*   </div> */}
+        <h2 className="py-4">What I Have Done</h2>
+        <div className="p-8 flex justify-center items-center">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 justify-center items-center">
+            <a href="https://github.com/jrmichell/jackm-portfolio">
+              <Card projectName={portfolioName} projectImg={portfolioImg} />
+            </a>
+            <a href="https://helvera.vercel.app/">
+              <Card projectName={helveraName} projectImg={helveraImg} />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
